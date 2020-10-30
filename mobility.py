9a35cb5566e20a758ba3f89d94c35ccd48916cb3
Tim <timothy.player@comcast.net>
2020-10-30 14:05:08 -0400

    Filtered mobility into fips and mobility.

--- mobility.py
+++ mobility.py
@@ -1,7 +1,17 @@
 import pandas as pd
 
 df = pd.read_csv('us-mobility.csv')
-
+fips = pd.read_csv('county_fips_master.csv',encoding = "ISO-8859-1")
+fips = fips.iloc[:,0:3]
 #Purges any line that contains Total to isolate counties
-df = df[df.A != "Total"]
-print(df.head(10))
+df = df[df != "Total"]
+
+
+df = df.dropna()
+fips = fips.rename(columns= {"county_name":"county"})
+df = df[(df['date'] == '2020-04-08')]
+
+
+
+r = df.merge(fips)
+r = r.iloc[:,2:-1]
