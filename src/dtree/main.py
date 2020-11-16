d9da313a9310906d3aafbd22f4180c2e8da9e073
jmandzak <jmandzak@vols.utk.edu>
2020-11-15 21:56:44 -0500

    Update: Functioning decision tree and plots. Issue #1

--- src/dtree/main.py
+++ src/dtree/main.py
@@ -1,9 +1,10 @@
 from sklearn import tree
 from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import classification_report, confusion_matrix, plot_confusion_matrix
+from sklearn.metrics import confusion_matrix
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # read in master dataset
 df = pd.read_csv("../../dataset.csv")
@@ -16,66 +17,224 @@ X = df.iloc[:, :-1]
 Y = df.iloc[:, -1:]
 
 
-# comment/uncomment these two lines out depending on what tree you want
+############################
+#                          #
+#   COMMENT/UNCOMMENT      #
+#   BASED ON FEATURES      #
+#   YOU WANT               #
+#                          #
+############################
+
 # X = X.drop(columns="cases")
 # X = X.drop(columns="deaths")
+
+
 
 # split into train and test using kfold
 kf = StratifiedKFold(n_splits=5)
 
+# split training and test data
 train_indices = []
 test_indices = []
-scores = []
-feature_importance = []
 
 for train, test in kf.split(X, Y):
     train_indices.append(train)
     test_indices.append(test)
 
-total_tn = 0
-total_fp = 0
-total_fn = 0
-total_tp = 0
+# initialize variables to keep up with data
+feature_importance = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+    10: []
+}
+total_tn = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0
+}
+total_fp = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0
+}
+total_fn = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0
+}
+total_tp = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0
+}
+
+scores_by_depth = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+    10: []
+}
+
+
+############################
+#                          #
+#  ACTUAL CLASSIFICATION   #
+#                          #
+############################
 
 for i in range(len(train_indices)):
-    dt = tree.DecisionTreeClassifier(criterion="entropy")
-    x_train = X.iloc[train_indices[i]]
-    x_test = X.iloc[test_indices[i]]
-    y_train = Y.iloc[train_indices[i]]
-    y_test = Y.iloc[test_indices[i]]
 
-    dt.fit(x_train, y_train)
+    # test different depths up to 5
+    for j in range(10):
+        dt = tree.DecisionTreeClassifier(criterion="entropy", max_depth=j+1)
 
-    y_pred = dt.predict(x_test)
-    row1, row2 = confusion_matrix(y_test, y_pred)
-    total_tn += row1[0]
-    total_fp += row1[1]
-    total_fn += row2[0]
-    total_tp += row2[1]
+        # set up variables
+        x_train = X.iloc[train_indices[i]]
+        x_test = X.iloc[test_indices[i]]
+        y_train = Y.iloc[train_indices[i]]
+        y_test = Y.iloc[test_indices[i]]
 
-    disp = plot_confusion_matrix(dt, x_test, y_test,
-                                 cmap=plt.cm.Blues)
-    plt.show()
+        # create the tree
+        dt.fit(x_train, y_train)
 
-    print(confusion_matrix(y_test, y_pred))
-    print(classification_report(y_test, y_pred))
+        # make the prediction
+        y_pred = dt.predict(x_test)
 
-    scores.append(dt.score(x_test, y_test))
-    feature_importance.append(dt.feature_importances_)
-    print()
+        # record the statistics
+        row1, row2 = confusion_matrix(y_test, y_pred)
+        total_tn[j+1] += row1[0]
+        total_fp[j+1] += row1[1]
+        total_fn[j+1] += row2[0]
+        total_tp[j+1] += row2[1]
+        scores_by_depth[j+1].append(dt.score(x_test, y_test))
+    
+        # note the feature importance of tree with max depth
+        feature_importance[j+1].append(dt.feature_importances_)
 
+
+############################
+#                          #
+#   ACCURACY BY DEPTH      #
+#                          #
+############################
+
+# let's see the accuracy score for each depth
+accuracy_by_depth = []
+for val in scores_by_depth.values():
+    accuracy_by_depth.append(np.mean(val))
+
+# let's plot a bar graph to show accuracy by depth
+depth = [i+1 for i in range(10)]
+plt.plot(depth, accuracy_by_depth)
+plt.xlabel("Max Depth")
+plt.ylabel("Accuracy")
+if len(X.columns) < 17:
+    plt.title("Accuracy By Max Depth Without Cases & Deaths")
+else:
+    plt.title("Accuracy By Max Depth With Cases & Deaths")
+plt.show()
+plt.close()
+
+
+# Now that we know which depth is best,
+# get avg confusion matrix and feature
+# importance of the best depth
+best_depth = accuracy_by_depth.index(max(accuracy_by_depth)) + 1
+
+############################
+#                          #
+#   AVG CONFUSION MATRIX   #
+#                          #
+############################
+
+cf_matrix = np.array([[total_tn[best_depth]/5, total_fp[best_depth]/5], [total_fn[best_depth]/5, total_tp[best_depth]/5]])
+
+# code used from https://medium.com/@dtuk81/confusion-matrix-visualization-fc31e3f30fea
+# to make this custom confusion matrix plot
+group_names = ['True Neg','False Pos','False Neg','True Pos']
+group_counts = ["{0:0.2f}".format(value) for value in
+                cf_matrix.flatten()]
+group_percentages = ["{0:.2%}".format(value) for value in
+                     cf_matrix.flatten()/np.sum(cf_matrix)]
+labels = [f"{v1}\n{v2}\n{v3}" for v1, v2, v3 in
+          zip(group_names,group_counts,group_percentages)]
+labels = np.asarray(labels).reshape(2,2)
+sns.heatmap(cf_matrix, annot=labels, fmt='', cmap='Blues')
+if len(X.columns) < 17:
+    plt.title("Average Confusion Matrix Without Cases & Deaths")
+else:
+    plt.title("Average Confusion Matrix With Cases & Deaths")
+plt.show()
+plt.close()
+
+
+
+############################
+#                          #
+#   FEATURE IMPORTANCE     #
+#                          #
+############################
 
 # get the avg of each feature's importance for all the k folds
-avg_importance = [np.mean(k) for k in zip(*(row for row in feature_importance))]
+avg_importance = [np.mean(k) for k in zip(*(row for row in feature_importance[best_depth]))]
 features = list(X.columns)
 
-print("true negatives = " + str(total_tn / 5))
-print("true positives = " + str(total_tp / 5))
-print("false negatives = " + str(total_fn / 5))
-print("false positives = " + str(total_fp / 5))
+# replace _ and 'and' of feature names with \n
+for i in range (len(features)):
+    if '_and_' in features[i]:
+        features[i] = features[i].replace('_and_', '\n')
+    if '_' in features[i]:
+        features[i] = features[i].replace('_', '\n')
 
+# show off the bar graph of feature importance
+plt.close()
 plt.bar(features, avg_importance)
 plt.xlabel("Features")
 plt.ylabel("Importance")
-plt.title("Feature Importance")
+if len(X.columns) < 17:
+    plt.title("Feature Importance Without Cases & Deaths")
+else:
+    plt.title("Feature Importance With Cases & Deaths")
+plt.xticks(rotation=30)
 plt.show()
